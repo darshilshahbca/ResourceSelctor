3992fb5f65bae50cbfc628d7111e17c0ceef75e7
Darshil Shah <darshil.saphcm@gmail.com>
2018-03-19 00:38:05 +0530

    Initial Commit

--- app/src/main/java/com/example/x_men/fargments/MainActivity.java
+++ app/src/main/java/com/example/x_men/fargments/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.x_men.fargments;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -11,20 +16,50 @@ import com.example.android.fargments.R;
 public class MainActivity extends AppCompatActivity {
 
     private boolean mTablet;
+    private ViewGroup fragmentContainer;
+    public static final String TAG = "MainActivity-";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById (R.id.toolbar);
+        setSupportActionBar (toolbar);
 
-        ViewGroup fragmentContainer =
-                findViewById (R.id.detail_fragment_container);
+        fragmentContainer = (ViewGroup) findViewById (R.id.detail_fragment_container);
         mTablet = (fragmentContainer != null);
 
-        TextView tvOut = findViewById (R.id.textOut);
-        tvOut.setText ("Fragments side-by-side? = " + mTablet);
+        TextView tvOut = (TextView) findViewById (R.id.textOut);
+        tvOut.setText ("Fragments side-by-side? " + mTablet);
+
+        FloatingActionButton fab = findViewById (R.id.fab);
+
+        assert fab != null;
+
+        fab.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick(View view) {
+                viewDetailFragment ();
+            }
+        });
+
+
+
+    }
+
+    private void viewDetailFragment() {
+
+        if (mTablet) {
+            FragmentManager fragmentManager = getSupportFragmentManager ();
+            DetailActivityFragment fragment = new DetailActivityFragment ();
+            fragmentManager.beginTransaction ()
+                    .add (R.id.detail_fragment_container, fragment)
+                    .commit ();
+        } else {
+            Intent intent = new Intent (this, DetailActivity.class);
+            startActivity (intent);
+        }
+
 
     }
 
